0f67fde44da2becf6e92b575e0d473f8088c2274
tomoki <tomokikadotani2020@gmail.com>
2025-08-04 14:22:12 +0900

    Update base.js

--- src/sites/base.js
+++ src/sites/base.js
@@ -1,3 +1,9 @@
-module.exports = async function () {
-  console.log("📄 base.js はダミーです。特に処理はありません。");
-};
+const SPACEDATA = require("./SPACEDATA");
+const MODE = require("./MODE");
+// 他の企業をここに追加していく形式
+
+module.exports = [
+  SPACEDATA,
+  MODE,
+  // 例：MOV, DeepingSource など
+];
